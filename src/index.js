d9dc18ab003ce1124a0dd9b2bab8d941203df2fa
Rahul Raina <rahulraina711@gmail.com>
2021-03-29 23:54:20 +0530

    redefined

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import {bugAdded, bugRemoved, bugResolved} from './actions';
 console.log(store.getState());
 
 store.dispatch(bugAdded("A bug related to logging"));
+store.dispatch(bugAdded("Another bug"));
+store.dispatch(bugAdded("A bug related copying"));
+
 console.log(store.getState());
 
 store.dispatch(bugResolved(1));
